0e12aec410c5732e54e19e45389a7fd658984ef1
burtonyaboy <isaac.burton@ndus.edu>
2019-06-14 17:14:25 -0500

    Added the date modified

--- webscrape.py
+++ webscrape.py
@@ -13,14 +13,18 @@ def main():
     datasets.write(0,2,"File Size")
     row = 1
     column = 0
+
     for i in range(0,34):
         r = requests.get(f'https://gishubdata.nd.gov/search/type/dataset?sort_by=changed&page=0%2C{i}&q=search/type/dataset')
         soup = BeautifulSoup(r.content,'html.parser')
         for link in soup.find_all("h2", class_="node-title"):
-            print(f'Added: {link.get_text()}')
             datasets.write(row,column,link.get_text())
+            set = requests.get("https://gishubdata.nd.gov" + str(link.a.get('href')))
+            tomatosoup = BeautifulSoup(set.content,"html.parser")
+            datasets.write(row,column+1,tomatosoup.find("div",class_="field-name-field-modified-date").get_text())
             row += 1
-    wb.save('datasets.xls')    
+
+    wb.save('datasets.xls')
 
 if __name__=="__main__":
     main()
